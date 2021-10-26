1030c5c7032ead92adb4b1fdb6e3fb85e98cf9dd
llbernat <llbernat@gmail.com>
2021-10-26 09:41:35 +0200

    Modified ml_project template

--- add_new_content.sh
+++ add_new_content.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 cd ../musi-aa-modulo1
-git pull https://github.com/juanhuguetgarcia/musi-aa-modulo1.git
+git pull
+cd ../ml_project_template
+git pull
 cd ../MUSI-AA
 
 rsync --archive --ignore-existing --verbose ../musi-aa-modulo1 ./ --exclude .git/ --exclude .gitignore
+
+# rsync --archive --ignore-existing --verbose ../ml_project_template ./ --exclude .git/ --exclude .gitignore
